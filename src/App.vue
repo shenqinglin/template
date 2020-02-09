15b7dff3db094b2cad7146e81b224d4bec542f45
zhangchao <zhangchao@carelink.cn>
2020-02-09 20:00:02 +0800

    句话

--- src/App.vue
+++ src/App.vue
@@ -141,7 +141,7 @@ export default {
           const shareLink = _this.createShareUrl()
           const shareTitle = '新冠肺炎居家自测工具'
           const sharePic = 'http://img.114yygh.com/static/2019ncov/img/slt.jpg'
-          const shareDesc = '官方权威工具，来做个小测试吧'
+          const shareDesc = '官方权威工具，来做个小测试吧。'
           window.wx.updateTimelineShareData({
             title: shareTitle, // 分享标题
             link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
@@ -166,7 +166,7 @@ export default {
           })
         })
       }).then(data => {
-        console.log('data -> ready')
+        // console.log('data -> ready')
         window.wx.ready(function () {
           // 分享到朋友圈
           // const shareLink = location.origin + '/activity/q1'
